cc59d5112353a19d4cf5c25c27527c8fc583f12d
Ankit Daftery <ankitdaf@gmail.com>
2012-08-30 00:38:57 +0530

    Added python client

---
+++ python_client/README.md
@@ -0,0 +1,42 @@
+Wireless Keyboard : Python Client
+=================================
+
+This folder contains simple python shell based clients for WirelessKeyboard
+
+sample_client serves as a basic hint to the flow of communication
+
+wikey.py is a fully working client, use it communicate to WirelessKeyboard
+
+wireless_keyboard.cfg is a configuration file that stores wifi connection information.Ideally, that should be the only file you would need to edit to setup the connection
+
+Invocation
+----------
+
+```
+$ ./wikey.py "your message goes here"
+```
+
+Additionally, to use it as a installed command like:
+
+```
+$ wikey "your message goes here" 
+```
+
+1.Rename wikey.py to wikey
+2.Copy wikey and wireless_keyboard.cfg to /usr/local/bin
+3.Type away
+
+Wishlist
+--------
+
+1. Do away with the exclamation marks in the message
+2. Provide a text based menu interface
+
+Project Page
+------------
+
+The project page can be found at http://ankitdaf.com/projects/WirelessKeyboard
+
+Feel free to fork,branch out, contribute and hack away !
+
+Ping me at me@ankitdaf.com if you wish to appreciate/criticize/contribute to the project
---
+++ python_client/wikey.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+"""
+Copyright Ankit Daftery,2012
+"""
+
+import sys,ConfigParser
+from socket import socket, AF_INET, SOCK_STREAM
+    
+config = ConfigParser.RawConfigParser()
+configfile = 'wireless_keyboard.cfg'
+
+def send(text,host):
+    """
+    Handles the sockets and data packet transmission
+    """
+    mySocket = socket( AF_INET, SOCK_STREAM )
+    mySocket.connect(host)
+    mySocket.sendto(text,host)
+    mySocket.close()
+    
+def main(argv=None):
+    """
+    Calls the shots
+    """
+    if(not config.read(configfile)):
+        create_config()
+    else:
+        host = fetch_config()
+    if(len(sys.argv)>1):
+        msg = ' '.join(sys.argv[1:])
+        send(msg,host)
+    
+def setup_config():
+    """
+    Generate configuration files if they don't exist
+    """
+    config.add_section('wifi')
+    config.set('wifi','ip_address','192.168.2.3')
+    config.set('wifi','port',26015)
+    with open('wireless_keyboard.cfg','wb') as configfile:
+        config.write(configfile)
+
+def fetch_config():
+    """
+    Retrieve configuration from file
+    """
+    config.read(configfile)
+    SERVER_IP = config.get("wifi","ip_address")
+    PORT = int(config.get("wifi","port"))
+    return (SERVER_IP,PORT)
+
+if __name__ == "__main__":
+  sys.exit(main())
