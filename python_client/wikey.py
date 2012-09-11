9c15545e5373bc825a42d162bff3ae9801330a22
Ankit Daftery <ankitdaf@gmail.com>
2012-09-11 19:20:12 +0530

    Refactored code,nothing major

--- python_client/wikey.py
+++ python_client/wikey.py
@@ -10,27 +10,32 @@ from socket import socket, AF_INET, SOCK_STREAM
 config = ConfigParser.RawConfigParser()
 configfile = 'wireless_keyboard.cfg'
 
-def send(text,host):
+class Wikey:
+  """
+  A class to handle the wireless connection
+  """
+  host=None  
+  def __init__(self):
+    """
+    Initialise the connection
+    """
+    if(not config.read(configfile)):
+      self.create_config()
+    self.host=self.fetch_config()
+    if(len(sys.argv)>1):
+      msg = ' '.join(sys.argv[1:])
+      self.send(msg)    
+  
+  def send(self,text):
     """
     Handles the sockets and data packet transmission
     """
     mySocket = socket( AF_INET, SOCK_STREAM )
-    mySocket.connect(host)
-    mySocket.sendto(text,host)
+    mySocket.connect(self.host)
+    mySocket.sendto(text,self.host)
     mySocket.close()
     
-def main(argv=None):
-    """
-    Calls the shots
-    """
-    if(not config.read(configfile)):
-        create_config()
-    host = fetch_config()
-    if(len(sys.argv)>1):
-        msg = ' '.join(sys.argv[1:])
-        send(msg,host)
-    
-def create_config():
+  def create_config(self):
     """
     Generate configuration files if they don't exist
     """
@@ -38,16 +43,17 @@ def create_config():
     config.set('wifi','ip_address','192.168.2.3')
     config.set('wifi','port',26015)
     with open('wireless_keyboard.cfg','wb') as configfile:
-        config.write(configfile)
+      config.write(configfile)
 
-def fetch_config():
-    """
-    Retrieve configuration from file
-    """
-    config.read(configfile)
-    SERVER_IP = config.get("wifi","ip_address")
-    PORT = int(config.get("wifi","port"))
-    return (SERVER_IP,PORT)
+  def fetch_config(self):
+      """
+      Retrieve configuration from file
+      """
+      config.read(configfile)
+      SERVER_IP = config.get("wifi","ip_address")
+      PORT = int(config.get("wifi","port"))
+      return (SERVER_IP,PORT)
 
 if __name__ == "__main__":
-  sys.exit(main())
+  wikey=Wikey()
+  sys.exit()
